87de91882ef168a9419b99be9b3c5708bfcb6226
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-16 16:55:38 +0500

    Add files via upload

---
+++ task21.js
@@ -0,0 +1,6 @@
+"use strict";
+// task 21 
+// think of something you could store in a typescript object.
+// write a program that creats objects containing these items.
+let person = { name: "Bushra", fname: "female", age: 27 };
+console.log(person);
---
+++ task21.ts
@@ -0,0 +1,5 @@
+// task 21 
+// think of something you could store in a typescript object.
+// write a program that creats objects containing these items.
+let person: {name: string , fname: string, age: number} = {name: "Bushra", fname:"female",age:27}
+console.log(person)
